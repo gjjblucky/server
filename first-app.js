4f4207a0b18476b992e0476cbeef28d0e6b16f0a
gjjblucky <pahadion007@gmail.com>
2022-04-10 16:16:05 +0530

    done

---
+++ first-app.js
@@ -0,0 +1,2 @@
+const fs=require('fs');
+fs.writeFileSync('helo.txt','helo from Node.js');
---
+++ server.js
@@ -0,0 +1,7 @@
+  const http=require('http');
+
+ const server= http.createServer((req,res)=>{
+      res.write('<h1>lucky rathoure</h1>');
+      res.end();
+  })
+  server.listen(4000);

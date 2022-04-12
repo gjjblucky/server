c63150666340abe0e4ab1a37a45f2d5850c16fe8
gjjblucky <pahadion007@gmail.com>
2022-04-13 01:14:32 +0530

    done

--- routingreq.js
+++ routingreq.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 
 const routes=require('./routes')
@@ -8,3 +8,30 @@ console.log(routes.someText);
 const server = http.createServer(routes.handler);
 
 server.listen(8000);
+
+
+-----------*/
+
+//express
+
+// const http = require('http');
+
+const express=require('express');
+
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log('in the middelware');
+    next();   //allow the request to continue to the next middelware in line
+});
+
+app.use((req,res,next)=>{
+    console.log('in another middelware');
+    res.send('<h1>hello from express js</h1>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
+

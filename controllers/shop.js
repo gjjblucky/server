1b0c0a48651d25110f1e43d7235110ad42e1b704
gjjblucky <pahadion007@gmail.com>
2022-04-29 03:46:57 +0530

    done

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,31 +2,28 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows,fieldData]) =>{
+  Product.findAll()
+  .then(products =>{
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
   })
   .catch(err => { console.log(err)});
- 
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product]) => {
-    
+  Product.findById(prodId )
+  .then(product => {  
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
-    
   })
-  .catch(err => console.log(err));
-  
+  .catch(err => console.log(err));  
 };
 
 exports.getIndex = (req, res, next) => {

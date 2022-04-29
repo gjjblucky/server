37a81821dbe7ea7a14bfcd79fc71360e87c06171
gjjblucky <pahadion007@gmail.com>
2022-04-29 07:02:43 +0530

    hdg

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const errorController = require('./controllers/error');
 const sequelize=require('./util/database');
 const Product=require('./models/product');
 const User=require('./models/user');
+const Cart=require('./models/cart');
+const CartItem=require('./models/cart-item');
 
 const app = express();
 
@@ -60,7 +62,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{costraints: true,onDelete: 'CASCADE'});
 User.hasMany(Product);
-
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through: CartItem});
+Product.belongsToMany(Cart,{through: CartItem});
 
 
 sequelize
@@ -77,9 +82,12 @@ if(!user){
 }
 return user;
 })
-.then(user => {
+.then(user=> {
+    return user.createCart();
     // console.log(user);
-    app.listen(3000);
+   
+}).then(cart =>{
+    app.listen(4000);
 })
 .catch(err => {
     console.log(err);
